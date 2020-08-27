7c9bc11f06b5b43b309f3e4e12266b4ec39120c2
johny anastacio <johnyanastaciods@gmail.com>
2020-08-27 06:16:24 -0300

    proposals created

--- src/controllers/Proposals/proposal.js
+++ src/controllers/Proposals/proposal.js
@@ -1,4 +1,5 @@
 const knex = require('../../database/connection');
+const { andWhere } = require('../../database/connection');
 
 exports.create = async (req, res) => {
     const candidate_id = req.params.candidate_id;
@@ -18,4 +19,59 @@ exports.create = async (req, res) => {
           })
 
     
+};
+
+exports.read = (req, res) => {
+    const candidate_id = req.headers.candidate_id;
+
+    knex('proposals').where('candidate_id',candidate_id).select('id','body')
+    .then(proposal => {
+        res.status(200).json(proposal)
+    }).catch(() =>{
+        res.status(400).json({error:"NO PROPOSALS"})
+    })
+}
+
+
+
+exports.update = (req, res) => {
+    const candidate_id = req.params.candidate_id;
+    const { body } = req.body;
+    const proposal_id = req.headers.proposal_id;
+
+
+    const proposal = {
+        body,
+        candidate_id
+    }
+
+    knex('proposals').select('*').where('id', proposal_id).andWhere('candidate_id', candidate_id)
+    .then(data =>{
+        if(data.length !== 0){
+            return knex('proposals').where('id', proposal_id).update('body',body)
+                    .then(() =>{
+                        res.status(200).json({msg:"PROPOSAL UPDATED"})
+                    })
+        }else{
+            return res.json({msg:"THERE IS NO PROPOSAL TO UPDATE"});
+        }
+    })
+}
+exports.remove = (req, res) => {
+    const candidate_id = req.params.candidate_id;
+    const proposal_id = req.headers.proposal_id;
+
+
+    knex('proposals').select('*').where('id',proposal_id).andWhere('candidate_id', candidate_id)
+    .then(data => {
+        if(data.length !== 0){
+            return knex('proposals').where('id',proposal_id).delete()
+            .then(() =>{
+                res.status(200).json({});
+            })
+
+        }else{
+            return res.status(404).json({error:"ERROR TO DELETE, VERIFY IF YOU ARE ABLE"});
+        }
+    })
 }
--- src/routes/proposalRoute.js
+++ src/routes/proposalRoute.js
@@ -1,10 +1,12 @@
 const route = require('express').Router();
 const auth = require('../middlewares/auth');
 const {userId, isAuth} = require('../middlewares/candidateAuth');
-const {create} = require('../controllers/Proposals/proposal');
+const {create,read} = require('../controllers/Proposals/proposal');
 
 
 route.post('/proposals/create/:candidate_id', auth, isAuth, create);
+route.get('/proposals',read );
+
 
 
 route.param('candidate_id', userId);
