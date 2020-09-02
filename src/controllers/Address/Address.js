8bdd9838d5ccf63b3ee097a8e1874af10507e00e
johny anastacio <johnyanastaciods@gmail.com>
2020-09-01 23:16:55 -0300

    state and city added

--- src/controllers/Address/Address.js
+++ src/controllers/Address/Address.js
@@ -14,3 +14,15 @@ exports.getState = async(req, res) => {
 
 
 }
+exports.getCity = async(req, res) => {
+     
+    const state_id = req.params.state_id;
+    await  knex('cidades').select('*').where('estado_id', state_id)
+    .then(cityData => {
+        if(cityData.length !== 0) {
+            res.status(200).json(cityData);
+        }else{
+            res.status(404).json("ERROR TO GET CITY")
+        }
+    })
+}
--- src/routes/addressRoute.js
+++ src/routes/addressRoute.js
@@ -1,7 +1,7 @@
 const route = require('express').Router();
-const  {getState} = require('../controllers/Address/Address')
+const  {getState, getCity} = require('../controllers/Address/Address')
 
 
 route.get('/state', getState);
-
+route.get('/city/:state_id', getCity);
 module.exports = route
