79a0f6c8b9ec426f2209c9f2fa00212fdaf12bee
Eric Pereira <ericiclone@gmail.com>
2020-09-21 19:05:41 -0300

    add compression

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const compression = require('compression');
 
 const app = express();
+
+app.use(compression());
 
 const path = require('path')
 //const morgan = require('morgan');
